d122cd138453b24ac2bbe9a20bc3729fc5bd6582
Renan Augusto <renan.augusto18@hotmail.com>
2018-08-14 22:10:04 -0300

    Login page

--- productmanager/routes.py
+++ productmanager/routes.py
@@ -5,3 +5,7 @@ home_api = Blueprint('home_api', __name__, template_folder='templates')
 @home_api.route("/")
 def hello():
     return "Hello World!"
+
+@home_api.route('/login')
+def login():
+    return render_template('login.html')
